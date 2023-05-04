b1b1bcbed444dba7d1a6acc0bf9e3ed79806b655
Jobijollof <jobinaibegbulem@yahoo.com>
2023-05-04 02:32:48 +0100

    python variables and datatypes

---
+++ Datatypes.py
@@ -0,0 +1,16 @@
+#Types of Strings:
+#Strings can be represented by
+STR1 = "double quotes"
+STR2 = 'single quotes'
+STR3 = '''Three single quotes'''
+STR4 = """ Double quotes as strings"""
+
+# Numbers:
+Num = 24 #integer
+Num = 6.7 #float
+
+#List: 
+# This is a Collection of variables. It is also called an array in other programming languages.
+# List can contain multiple data types. A float, integer or string can be in a list.
+# Lists are enclosed in square brackets.
+# All elements separated by a comma spacing is not mandatory
---
+++ Variables.py
@@ -0,0 +1,25 @@
+# Variable Assignment
+
+Var1 = 75  # Variable Integer
+Var2 = 'Anime' # Variable String
+Var3 = 3.6   #Variable float
+
+# Printing Variables
+
+print (type(Var1)) #outputs data type class 'int'
+print(Var2)
+print(Var3)
+
+# Multiple Assignments
+a = b = c = 78
+
+print(a)
+print(b)
+print(c) #all the output is 78 because all the variable have the same value
+
+# Multiple values assigned to multiple variables
+x, y, z = "xenophobic", 8.2, 3, #example of positional arguments. 
+
+print("The value of the variable x is" , x ) #Comma is a separator
+print("The value of the variable y is" , y )
+print("The value for z is" , z)
