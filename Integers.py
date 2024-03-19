f70d38de3b7cc6e81b42e1372d119e027744509e
Ifyuduh <ifyuduh@gmail.com>
2024-03-19 13:28:01 -0600

    commit 2

---
+++ Integers.py
@@ -0,0 +1,19 @@
+age = 30
+score = -85
+high_score = 1000000 # Large integers are also supported
+
+# Zero is also an integer
+count = 0
+
+#Performing Operations with Integers: Python supports various arithmetic operations on integers:
+# Addition
+total = age + score
+print(total) # Output: -55
+
+#Subtraction
+difference = high_score - score
+print(difference) # Output: 1000085
+
+# Multiplication
+product = age * count
+print(product) # Output: 0
--- printing.py
+++ printing.py
@@ -8,7 +8,27 @@ print(f"My name is {name} and i am a {profession}") #Using the f string
 
 
 # Concatenation
+#Concatenation in Python refers to the process of combining two or more strings
+# together to create a single string. 
+#This operation is commonly performed using the + operator or the += operator
 
+str1 = "Hello"
+str2 = "World"
+result = str1 + ", " + str2
+print(result) # Output: Hello, World
+
+
+#You can also use the += operator to concatenate strings and assign the result back to a variable:
+
+str1 = "Hello"
+str2 = "World"
+str1 += ", " + str2
+print(str1) # output: Hello, World
+
+#Additionally, you can use the join() method to concatenate multiple strings from a list:
+strings = ["Hello", "World"]
+result = ", ".join(strings)
+print(result) # Output: Hello, World 
 
 
 
